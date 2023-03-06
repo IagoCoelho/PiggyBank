4a7f0c6a0d58836b673da330c3f1784c489b0909
IagoCoelho <iagocoelhos@gmail.com>
2023-03-06 20:54:08 -0300

    Spring FrameWork

---
+++ src/main/java/br/com/fiap/piggybank/controllers/DespesaController.java
@@ -0,0 +1,26 @@
+package br.com.fiap.piggybank.controllers;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.piggybank.models.Despesa;
+import java.time.LocalDate;
+
+@RestController
+public class DespesaController {
+    
+    @GetMapping("/api/despesas")
+    public Despesa show(){
+        var despesa = new Despesa(
+                    new BigDecimal(100),
+                    LocalDate.now(),
+                    "cinema"
+                );
+
+        return despesa;
+    }
+
+}
